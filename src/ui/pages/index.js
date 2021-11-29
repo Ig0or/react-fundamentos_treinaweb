2dcff3168c2e2636d417a2268a2a4a90679321f8
Ig0or <igorprovensi@gmail.com>
2021-11-29 12:28:54 -0300

    criacao do projeto

---
+++ src/ui/pages/index.js
@@ -0,0 +1,6 @@
+import { useIndex } from "../../data/hooks/pages/useIndex.page";
+import styles from "../styles/pages/index.module.css";
+
+export default function Index() {
+    return <div>asasasasas</div>;
+}
