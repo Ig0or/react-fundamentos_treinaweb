8f0e2a50b4391eedf98e383a79ec675d3a3bc172
Ig0or <igorprovensi@gmail.com>
2021-11-29 14:12:10 -0300

    adicionado componente tweet

--- src/ui/pages/index.js
+++ src/ui/pages/index.js
@@ -1,6 +1,23 @@
 import { useIndex } from "../../data/hooks/pages/useIndex.page";
 import styles from "../styles/pages/index.module.css";
+import TextInput from "../components/inputs/TextInput/TextInput";
+import Tweet from "../components/data-display/Tweet/Tweet";
+
+const tweet = {
+    date: "há 5 dias",
+    text: "Meu primeiro tweet",
+    user: {
+        name: "Ig0or",
+        username: "Ig0or",
+        picture: "https://github.com/Ig0or.png",
+    },
+};
 
 export default function Index() {
-    return <div>asasasasas</div>;
+    return (
+        <div>
+            <TextInput />
+            <Tweet tweet={tweet} />
+        </div>
+    );
 }
